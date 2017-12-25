91417df085e4e026c76ed369396ae7cc51f0879c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-25 21:03:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    binding.pry
     @user = User.find_by(username: params[:username], password: params[:password])
     session[:user_id] = @user.id
+    binding.pry
     redirect '/account'
   end
 
