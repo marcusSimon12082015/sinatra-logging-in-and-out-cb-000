92924f8f7d05aad4e974f3e26039b071706f12e9
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-25 21:22:14 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,8 +22,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
+    if Helpers.is_logged_in?(session)
+      @user = User.find(session[:user_id])
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
