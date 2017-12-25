429d94097f30362b5b9705df1e6a575ee2e9e16c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-25 20:37:03 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20171225201316) do
 
   create_table "users", force: :cascade do |t|
-    t.string  "name"
+    t.string  "username"
     t.string  "password"
     t.integer "balance"
   end
