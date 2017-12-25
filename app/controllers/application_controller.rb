2b6bfc0a8034badf0f94c0e601be7cfe629ef914
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-25 21:37:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,29 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username], password: params[:password])
+    #binding.pry
+    if @user.nil?
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      @user = User.find(session[:user_id])
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.delete(:user_id)
+    redirect to '/'
   end
 
 
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+  def self.current_user(session)
+    @user = User.find(session[:user_id])
+    @user
+  end
+  def self.is_logged_in?(session)
+    session.key?(:user_id)
+  end
 end
